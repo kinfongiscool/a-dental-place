b22c8b5c199a51f60240f1c608e5ff755ca241ea
Bradley Kin Fong <kin@ministrycentered.com>
2017-12-07 18:05:39 -0600

    set up media.js

---
+++ src/media.js
@@ -0,0 +1,18 @@
+import { css } from 'styled-components';
+
+const sizes = {
+  giant: 1800,
+	desktop: 1200,
+	tablet: 900,
+	phone: 600
+};
+
+export const media = Object.keys(sizes).reduce((acc, label) => {
+	acc[label] = (...args) => css`
+		@media (max-width: ${sizes[label] / 16}em) {
+			${css(...args)}
+		}
+	`
+
+	return acc
+}, {});
