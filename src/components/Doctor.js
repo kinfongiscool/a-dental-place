28dc97dc2f46bff31924a9d8f98542c54243a5fb
Bradley Kin Fong <kin@ministrycentered.com>
2017-12-11 15:35:33 -0600

    doctor section media queries

--- src/components/Doctor.js
+++ src/components/Doctor.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 import styled, { css } from 'styled-components';
+import { media } from '../media.js';
 
 const DoctorContainer = styled.div`
   display: flex;
   flex-direction: ${props => props.imageLeft ? 'row' : 'row-reverse'};
   margin: ${props => props.imageLeft ? '0 0 2rem 0' : '2rem 0 0 0'};
+
+  ${media.phone`
+    display: inline-block;
+    margin: 0;
+  `}
 `;
 
 const DoctorImage = styled.img`
@@ -12,6 +18,10 @@ const DoctorImage = styled.img`
   z-index: 10;
   align-self: ${props => props.imageLeft ? 'flex-start' : 'flex-end'};
   background: #58A6FF;
+
+  ${media.phone`
+    align-self: center;
+  `}
 `;
 
 const DoctorTextContainer = styled.div`
@@ -23,16 +33,33 @@ const DoctorTextContainer = styled.div`
   left: ${props => props.imageLeft ? '' : '3rem'};
   background-color: #EBF4FF;
   align-items: left;
-  padding: ${props => props.imageLeft ? '.5rem 1.5rem .5rem 4.5rem' : '.5rem 4.5rem .5rem 1.5rem'};
+  padding: ${props => props.imageLeft ? '1rem 2rem 1rem 5rem' : '1rem 5rem 1rem 2rem'};
   margin: ${props => props.imageLeft ? '0 -2rem 2rem 0' : '1rem 0 0 -2rem'};
+
+  ${media.phone`
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    padding: 2rem;
+    margin: 1rem 2rem 2rem 2rem;
+  `}
 `;
 
 const DoctorName = styled.h3`
   text-align: left;
+
+  ${media.phone`
+    margin: 0 0 1rem 0;
+  `}
 `;
 
 const DoctorDescription = styled.p`
   text-align: left;
+
+  ${media.phone`
+    margin: 0;
+  `}
 `;
 
 class Doctor extends Component {
