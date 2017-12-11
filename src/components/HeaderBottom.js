17cc6c295b7783fa1fe404ccff1f810bc69a5d36
Bradley Kin Fong <kin@ministrycentered.com>
2017-12-11 15:39:06 -0600

    header nav media query

--- src/components/HeaderBottom.js
+++ src/components/HeaderBottom.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';
 import styled, { css } from 'styled-components';
+import { media } from '../media.js';
 
 const HeaderBottomContainer = styled.div`
   background: #58A6FF;
 `;
 
 const HeaderBottomNavList = styled.ul`
+  padding: 0;
   margin: 0;
   list-style: none;
   display: flex;
   justify-content: center;
+
+  ${media.phone`
+    display: inline-block;
+  `}
 `;
 
 const HeaderBottomNavItem = styled.li`
