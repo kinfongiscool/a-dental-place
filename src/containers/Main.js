cab871732d3ea79e9646946a551ab7dc570e9611
Bradley Kin Fong <kin@ministrycentered.com>
2017-12-24 00:32:42 -0600

    add instructions and download links

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -3,7 +3,7 @@ import { Switch, Route } from 'react-router-dom';
 import Home from './Home.js';
 import MeetTheTeam from './MeetTheTeam.js';
 import Services from './Services.js';
-import NewPatients from './NewPatients.js';
+import NewPatientsContainer from './NewPatientsContainer.js';
 import Faqs from './Faqs.js';
 import Contact from './Contact.js';
 import Testimonials from './Testimonials.js';
@@ -14,7 +14,7 @@ const Main = () => (
       <Route exact path='/' component={ Home }/>
       <Route path='/meet-the-team' component={ MeetTheTeam }/>
       <Route path='/services' component={ Services }/>
-      <Route path='/new-patients' component={ NewPatients }/>
+      <Route path='/new-patients' component={ NewPatientsContainer }/>
       <Route path='/faqs' component={ Faqs }/>
       <Route path='/contact' component={ Contact }/>
       <Route path='/testimonials' component={ Testimonials }/>
